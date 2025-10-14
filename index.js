cf4580cac39999e3cf1fb35de867a3c48e68a8f4
ruksh-mach49 <ruksh.rao@mach49.com>
2025-10-14 16:09:46 +0000

    fixed import bug

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import cron from 'node-cron';
-import { handler } from './print_sports';
+import { handler } from './print_sports.js';
 import http from 'http';
 
 const server = http.createServer((req, res) => {
